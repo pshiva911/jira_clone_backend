54209f0d26a78aa45da5512ecb0eb66ffabb8d86
pshiva911 <shivakrishna911@gmail.com>
2024-10-01 11:31:48 +0530

    change samesite to none

--- controllers/util.js
+++ controllers/util.js
@@ -41,6 +41,6 @@ const updateOrder = async ({ id, order, type, model }) => {
 
 const badRequest = (res) => res.status(400).json({ message: 'Whoop! Looks like your http request is missing something.' }).end()
 
-const cookieConfig = { path : '/',maxAge: 60 * 60 * 24 * 15,httpOnly: true , secure : true , sameSite : "strict"}
+const cookieConfig = { path : '/',maxAge: 60 * 60 * 24 * 15,httpOnly: true , secure : true , sameSite : "none"}
 
 module.exports = { sameContainerReorder, diffContainerReorder, badRequest, cookieConfig }
