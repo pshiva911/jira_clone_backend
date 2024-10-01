f013e5ad6275f64cf5556bcd0e68534ffc45d279
pshiva911 <shivakrishna911@gmail.com>
2024-10-01 21:12:49 +0530

    new changes

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ const corsOptions = {
 	  if (whitelist.indexOf(origin) !== -1 || !origin) {
 		callback(null, true)
 	  } else {
-		callback(new Error('Not allowed by CORS'))
+		callback(new Error('Not allowed by CORS'),true)
 	  }
 	},
 	credentials: true
